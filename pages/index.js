6a4b68924f8107bc6a8b759950cd83b4ed56cc9c
romijatmiko <romijatmiko033@gmail.com>
2023-06-16 21:22:42 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -51,8 +51,7 @@ const Home = ({ inventoryData = [], categories: categoryData = [] }) => {
       <div className="pt-10 pb-6 flex flex-col items-center">
         <h2 className="text-4xl mb-3">Trending Now</h2>
         <p className="text-gray-600 text-sm">
-          Find the perfect piece or accessory to finish off your favorite room
-          in the house.
+          Selamat Berbelanja di Go Bakery ECommerce
         </p>
       </div>
       <div className="my-8 flex flex-col lg:flex-row justify-between">
